fe05f541c6e8c84b022a40e6c1421dbb23d127be
Uvarovskyi-Maksim <134094323+Uvarovskyi-Maksim@users.noreply.github.com>
2023-05-19 23:12:22 +0300

    Update DepartmentCreator.java

--- src/main/java/lab4/controller/DepartmentCreator.java
+++ src/main/java/lab4/controller/DepartmentCreator.java
@@ -1,2 +1,8 @@
-package lab4.controller;public class DepartmentCreator {
+package lab4.controller;
+import lab4.model.*;
+public class DepartmentCreator implements Creator<Department> {
+    @Override
+    public Department create(String name, Human header) {
+        return new Department(name, header);
+    }
 }
