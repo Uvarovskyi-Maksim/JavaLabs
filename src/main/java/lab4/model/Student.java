5edbdb43387f3764ed2b1da5e9e2e1d821d53349
Uvarovskyi-Maksim <134094323+Uvarovskyi-Maksim@users.noreply.github.com>
2023-05-19 23:10:47 +0300

    Update Student.java

--- src/main/java/lab4/model/Student.java
+++ src/main/java/lab4/model/Student.java
@@ -1,2 +1,7 @@
-package lab4.model;public class Student {
+package lab4.model;
+
+public class Student extends Human {
+    public Student(String firstName, String lastName, String parentage, String headerParentage, Sex gender) {
+        super(firstName, lastName, parentage, gender);
+    }
 }
