b4bcab7233c4361179d71884bc06a25de100ab25
Uvarovskyi-Maksim <134094323+Uvarovskyi-Maksim@users.noreply.github.com>
2023-05-19 23:13:18 +0300

    Update StudentCreator.java

--- src/main/java/lab4/controller/StudentCreator.java
+++ src/main/java/lab4/controller/StudentCreator.java
@@ -1,2 +1,8 @@
-package lab4.controller;public class StudentCreator {
+package lab4.controller;
+import lab4.model.*;
+public class StudentCreator implements Creator<Student> {
+    @Override
+    public Student create(String name, Human header) {
+        return new Student(name, header.getFirstName(), header.getLastName(), header.getParentage(), header.getGender());
+    }
 }
