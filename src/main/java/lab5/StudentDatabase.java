a2e126b43b7d99abd5945cfbef76223dd73d77ca
Uvarovskyi-Maksim <134094323+Uvarovskyi-Maksim@users.noreply.github.com>
2023-05-19 23:30:28 +0300

    Update StudentDatabase.java

--- src/main/java/lab5/StudentDatabase.java
+++ src/main/java/lab5/StudentDatabase.java
@@ -1,2 +1,48 @@
-package lab5;public class StudentDatabase {
+package lab5;
+import java.sql.*;
+
+public class StudentDatabase {
+    public static void main(String[] args) {
+        try {
+            
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/student");
+            System.out.println("Connected to the database!");
+
+           
+            int month = 5; 
+
+            
+            String sql = "SELECT * FROM students WHERE MONTH(date_of_birth) = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, month);
+            ResultSet resultSet = statement.executeQuery();
+
+            
+            System.out.println("\nStudents born in month " + month + ":");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String lastName = resultSet.getString("last_name");
+                String firstName = resultSet.getString("first_name");
+                String patronymic = resultSet.getString("patronymic");
+                Date dateOfBirth = resultSet.getDate("date_of_birth");
+                String recordBookNumber = resultSet.getString("record_book_number");
+
+                System.out.println("ID: " + id);
+                System.out.println("Last Name: " + lastName);
+                System.out.println("First Name: " + firstName);
+                System.out.println("Patronymic: " + patronymic);
+                System.out.println("Date of Birth: " + dateOfBirth);
+                System.out.println("Record Book Number: " + recordBookNumber);
+                System.out.println();
+            }
+
+           
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Error occurred while connecting to the database!");
+            e.printStackTrace();
+        }
+    }
 }
