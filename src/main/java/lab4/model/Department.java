331cbd53728a8fd4775a59559c12836ad1191f73
Uvarovskyi-Maksim <134094323+Uvarovskyi-Maksim@users.noreply.github.com>
2023-05-19 23:09:09 +0300

    Update Department.java

--- src/main/java/lab4/model/Department.java
+++ src/main/java/lab4/model/Department.java
@@ -1,2 +1,20 @@
-package lab4.model;public class Department {
+package lab4.model;
+
+public class Department {
+    private String name;
+    private Human head;
+
+    public Department(String name, Human head) {
+        this.name = name;
+        this.head = head;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Human getHead() {
+        return head;
+    }
 }
+
