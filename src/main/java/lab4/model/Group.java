c8ffdeadbe08ca949efe9609273a4e736a6d46d5
Uvarovskyi-Maksim <134094323+Uvarovskyi-Maksim@users.noreply.github.com>
2023-05-19 23:09:51 +0300

    Update Group.java

--- src/main/java/lab4/model/Group.java
+++ src/main/java/lab4/model/Group.java
@@ -1,2 +1,19 @@
-package lab4.model;public class Group {
+package lab4.model;
+
+public class Group {
+    private String name;
+    private Human groupLeader;
+
+    public Group(String name, Human groupLeader) {
+        this.name = name;
+        this.groupLeader = groupLeader;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Human getGroupLeader() {
+        return groupLeader;
+    }
 }
