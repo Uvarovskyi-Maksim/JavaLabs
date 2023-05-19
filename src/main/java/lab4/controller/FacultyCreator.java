6714a0d9fe02749998adff1bc1fe5663fe17a61e
Uvarovskyi-Maksim <134094323+Uvarovskyi-Maksim@users.noreply.github.com>
2023-05-19 23:12:36 +0300

    Update FacultyCreator.java

--- src/main/java/lab4/controller/FacultyCreator.java
+++ src/main/java/lab4/controller/FacultyCreator.java
@@ -1,2 +1,8 @@
-package lab4.controller;public class FacultyCreator {
+package lab4.controller;
+import lab4.model.*;
+public class FacultyCreator implements Creator<Faculty> {
+    @Override
+    public Faculty create(String name, Human header) {
+        return new Faculty(name, header);
+    }
 }
