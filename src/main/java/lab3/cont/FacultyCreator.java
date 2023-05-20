fb0a4bd45cb62b03f4ad3e32f085537b664d7001
Uvarovskyi-Maksim <134094323+Uvarovskyi-Maksim@users.noreply.github.com>
2023-05-20 17:01:52 +0300

    Update FacultyCreator.java

--- src/main/java/lab3/cont/FacultyCreator.java
+++ src/main/java/lab3/cont/FacultyCreator.java
@@ -1,5 +1,5 @@
-package org.example.cont;
-import org.example.mod.*;
+package lab3.cont;
+import lab3.mod.*;
 public class FacultyCreator implements Creator<Faculty> {
     @Override
     public Faculty create(String name, Human header) {
